2f4f6e1cd27b0f85bca42b9be06602c4e2d99343
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2022-03-18 18:38:56 -0300

    iniciando rentalx

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,7 @@
 import express from 'express';
-import { createCourse } from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World Ignite!" })
-});
-
-app.get("/create", createCourse);
-
-app.get("/message", (request, response) => {
-    return response.json({message:"Hello eslint e pretier"})
-});
-
-app.post("/courses", (request,response) => {
-    const { name, valor } = request.body;
-
-    const valor2 = 31;
-
-    return response.json({ name, valor });
-});
 
 app.listen(3333, () => console.log("Server is running on PORT: 3333"));
