fb6b71a49552ce188d3819a06976f0e9664e9018
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2022-03-18 18:24:16 -0300

    mudando para rentalx

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,24 @@ import { createCourse } from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
     return response.json({ message: "Hello World Ignite!" })
 });
 
 app.get("/create", createCourse);
 
-app.listen(3333, console.log("Server is running on PORT: 3333"));
+app.get("/message", (request, response) => {
+    return response.json({message:"Hello eslint e pretier"})
+});
+
+app.post("/courses", (request,response) => {
+    const { name, valor } = request.body;
+
+    const valor2 = 31;
+
+    return response.json({ name, valor });
+});
+
+app.listen(3333, () => console.log("Server is running on PORT: 3333"));
