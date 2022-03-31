9b16f7084f6b696da8b881c3a0be6804f6900886
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2022-03-31 00:48:50 -0300

    criado index para import

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 
 
 class ImportCategoryController {
+    constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+
     handle(request: Request, response: Response): Response {
         const { file } = request;
-        console.log(file);
+    
+        this.importCategoryUseCase.execute(file);
+
         return response.send();
     }
 }
